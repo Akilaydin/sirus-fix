e61a531ebd8c5b3ee021b5f1b7a96193947e9dc0
Artem Ovchinnikov <akilaydin2@gmail.com>
2026-03-15 16:13:07 +0300

    Create run-sirus-without-proxy.bat

---
+++ run-sirus-without-proxy.bat
@@ -0,0 +1,8 @@
+@echo off
+set "http_proxy="
+set "https_proxy="
+set "HTTP_PROXY="
+set "HTTPS_PROXY="
+set "NO_PROXY="
+set "no_proxy="
+start "" "%LocalAppData%\Programs\sirus-open-launcher\Sirus Launcher.exe"
